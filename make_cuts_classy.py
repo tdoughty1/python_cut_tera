ba0f4ff819979f0fd1f666a1fdb4c0134b95ef1c
Brett Cornell <brett.durcan@gmail.com>
2013-08-05 13:17:13 -0700

    rsync to production account on micro

--- make_cuts_classy.py
+++ make_cuts_classy.py
@@ -30,6 +30,7 @@ except:
 
 
 class rootcut(graphbuilder.depbuilder):
+
     """rootcut inherits from the depbulider class (which is found in the graphbuilder module).
     It is typically run as a script by calling its main method. From the python interpreter,
     that would look like:
@@ -43,28 +44,28 @@ class rootcut(graphbuilder.depbuilder):
         and sets some attributes which are fairly self explanatory"""
 
         self.force = False
-        #location of the root cuts (containing directories named for
-        #data taking mode--like ba or bg_restricted etc.)
+        # location of the root cuts (containing directories named for
+        # data taking mode--like ba or bg_restricted etc.)
         self.root_cutdir = (
             '/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/cuts/')
-        #cut generation dir
+        # cut generation dir
         self.root_cutdir_gen = '/tera2/data3/cdmsbatsProd/processing/cuts'
-        #location of checked out matlab cuts
+        # location of checked out matlab cuts
         self.mat_cutdir = ('/tera2/data3/cdmsbatsProd/processing'
-            '/cdmstools/CAP/FCCS/cuts/Soudan/r133')
-        #update cvs
+                           '/cdmstools/CAP/FCCS/cuts/Soudan/r133')
+        # update cvs
         print "Updating cvs"
         self.ret = self.update_cvs(self.mat_cutdir)
         if self.ret == 0:
             print "cvs updated successfully"
-        #run type list
+        # run type list
         self.run_type_list = ['ba', 'cf', 'bg_restricted', 'bg_permitted']
-        #make list of existing matlab cuts
+        # make list of existing matlab cuts
         self.new_cut_list = [
             i.rstrip('m').rstrip('.')
-            for i in (os.listdir(self.mat_cutdir)+os.listdir(
-                self.mat_cutdir+'/Prodv5-3')) if i.split('.')[-1] == 'm']
-        #make dict of cuts to update (a list for each running mode)
+            for i in (os.listdir(self.mat_cutdir) + os.listdir(
+                self.mat_cutdir + '/Prodv5-3')) if i.split('.')[-1] == 'm']
+        # make dict of cuts to update (a list for each running mode)
         self.update_dict = {}
         self.outdated_dict = {}
         self.old_version_dict = {}
@@ -74,9 +75,9 @@ class rootcut(graphbuilder.depbuilder):
         print "Initializing cut dependency graph..."
         graphbuilder.depbuilder.__init__(self)
 
-############################
+#
 #     Main method
-###########################
+#
 
     def main(self):
         """The main method. This is the method that is called that actually does everything.
@@ -85,20 +86,20 @@ class rootcut(graphbuilder.depbuilder):
         cuts that are outdated (as well as any reverse dependencys for that cut) and hands
         the cuts off to matlab for generation. It then makes logs and preforms the hardlink."""
 
-        #build ole root cut dictonary with keys that are the various running
-        #mode and values that are lists of cuts
+        # build ole root cut dictonary with keys that are the various running
+        # mode and values that are lists of cuts
         self.root_cut_dict = self.build_cutlists(
             self.root_cutdir_gen,
             self.run_type_list)
-        #make dict of cuts that need updating
+        # make dict of cuts that need updating
         for h in self.root_cut_dict:
-            #get new matlab cut list and check for new cuts
+            # get new matlab cut list and check for new cuts
             cuts_to_update = []
             for i in list(set(self.new_cut_list) - set(self.root_cut_dict[h])):
                 cuts_to_update.append(i)
-            #make a list of all outdated cuts
+            # make a list of all outdated cuts
             print "Building list of outdated cuts for {}".format(h)
-            if self.user_cuts == None:
+            if self.user_cuts is None:
                 self.outdated_dict[h] = self.make_cut_update_list(
                     self.mat_cutdir,
                     self.root_cut_dict[h],
@@ -108,7 +109,7 @@ class rootcut(graphbuilder.depbuilder):
             if self.outdated_dict[h] is None:
                 self.outdated_dict[h] = []
             self.update_dict[h] = self.outdated_dict[h]
-        #stop if there are no cuts to update
+        # stop if there are no cuts to update
         if ([self.update_dict[i] for i in self.run_type_list] == [[] for i in self.run_type_list]):
             print "It looks like all the cuts are up to date"
             return 'It looks like all the cuts are up to date'
@@ -116,16 +117,16 @@ class rootcut(graphbuilder.depbuilder):
             print "The following cuts need to be updated:"
             for i in self.update_dict:
                 print "{} cuts:".format(i)
-                for j,k in self.update_dict[i]:
-                    print "Cut {} will be updated to version {}".format(j,k)
-            #kludge
+                for j, k in self.update_dict[i]:
+                    print "Cut {} will be updated to version {}".format(j, k)
+            # kludge
             kludge = {
                 i: {
                     j: k for j, k in self.update_dict[i]}
-                     for i in self.update_dict}
-            #print kludge
-            #hand cuts off to Matlab
-            #try:
+                for i in self.update_dict}
+            # print kludge
+            # hand cuts off to Matlab
+            # try:
             print "Updating CAP"
             self.update_cvs(self.root_cutdir_gen + '/../cdmstools')
             print "Building FCCS tree"
@@ -134,13 +135,9 @@ class rootcut(graphbuilder.depbuilder):
 
             return 'Matlab cut update script run and retuned ', mout
 
-
-
-######################################
+#
 #       Helper methods
-######################################
-
-
+#
     def produce(self, kludge):
         """A wrapper method that contains the matlab interface. This is where the structure of my script's
         concurrency can be changed (what loops when and where.). It also calls the two rsync commands that get the cuts
@@ -148,40 +145,46 @@ class rootcut(graphbuilder.depbuilder):
 
         for types in self.run_type_list:
             print "Updating {} cuts:".format(types)
-            arg1 = '/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/byseries/{}'.format(types)
+            arg1 = '/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/byseries/{}'.format(
+                types)
             arg2 = '/tera2/data3/cdmsbatsProd/processing/cuts/{}'.format(types)
             arg3 = kludge[types]
             print "Removing cuts no longer in CVS..."
-            for old_cut in (cut for cut in os.listdir(self.root_cutdir_gen +"/{}".format(types)) if cut not in self.new_cut_list):
+            for old_cut in (cut for cut in os.listdir(self.root_cutdir_gen + "/{}".format(types)) if cut not in self.new_cut_list):
                 print "Removing {}".format(old_cut)
-                shutil.rmtree(self.root_cutdir_gen + '/{}/{}'.format(types, old_cut))
+                shutil.rmtree(
+                    self.root_cutdir_gen +
+                    '/{}/{}'.format(
+                        types,
+                        old_cut))
+
             def mapper(cvb):
-                c,v,b = cvb
+                c, v, b = cvb
 
                 print "===> {}".format(c)
                 if b == 'before':
-                    #if old cuts exist, delete them
+                    # if old cuts exist, delete them
                     direc = self.root_cutdir_gen + "/{}/{}".format(types, c)
                     if os.path.isdir(direc):
                         print "-> Removing old {} cuts...".format(c)
                         shutil.rmtree(direc)
-                #print "-> Generating new {} cuts ...".format(c)
+                # print "-> Generating new {} cuts ...".format(c)
                 else:
                     if (mout == 0) and (self.version_from_rootfile(c, self.root_cutdir_gen, types) == v):
                         mresult = '{} build: Success!'.format(c)
                     else:
-                        mresult  = '{} build: Failure!'.format(c)
+                        mresult = '{} build: Failure!'.format(c)
                     print "-> {}".format(mresult)
             cvlist = []
-            for c,v in arg3.iteritems():
+            for c, v in arg3.iteritems():
                 cvlist.append(c)
                 cvlist.append(v)
 
-            map(mapper, ((c,v,'before') for c,v in arg3.iteritems()))
+            map(mapper, ((c, v, 'before') for c, v in arg3.iteritems()))
             print "Handing cuts off to MATLAB for production."
             arguements = [arg1, arg2] + cvlist
-            mout = self.matlab_fork("makeROOTcut",*arguements)
-            map(mapper, ((c,v,'after') for c,v in arg3.iteritems()))
+            mout = self.matlab_fork("makeROOTcut", *arguements)
+            map(mapper, ((c, v, 'after') for c, v in arg3.iteritems()))
         self.make_logs(self.update_dict, self.root_cutdir_gen)
         self.hlinker(self.root_cutdir_gen, self.root_cutdir)
         self.linkupdate()
@@ -194,8 +197,8 @@ class rootcut(graphbuilder.depbuilder):
         os.chdir(self.root_cutdir)
         if os.path.islink(self.root_cutdir + '/' + "current"):
             ret1 = subprocess.call(["rm", "current"])
-        ret = subprocess.call(["ln", "-s",  ndir,  "current"])
-        return ret,ret1
+        ret = subprocess.call(["ln", "-s", ndir, "current"])
+        return ret, ret1
 
     def rsyncer(self):
         """Calls the rsync command to copy the data to nero"""
@@ -205,7 +208,7 @@ class rootcut(graphbuilder.depbuilder):
             "rsync",
             "-avr",
             "/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/cuts/",
-            "cdms@cdmsmicro.fnal.gov:/micro/data6/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/cuts/"
+            "cdmsmicro.fnal.gov:/micro/data6/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/cuts/"
         ])
 
         print "rsync'ing data to nero..."
@@ -224,7 +227,7 @@ class rootcut(graphbuilder.depbuilder):
             "/tera2/data3/cdmsbatsProd/R133/dataReleases/Prodv5-3_June2013/merged/cuts/",
             "cdmsonly@galba.stanford.edu:/data/R133/dataReleases/Prodv5-3_June2013/merged/cuts/"
         ])
-        return ret1,ret2
+        return ret1, ret2
 
     def update_cvs(self, mat_cut_dir):
         """Calls the 'cvs update' command in a subprocess. If the subprocess
@@ -239,9 +242,9 @@ class rootcut(graphbuilder.depbuilder):
                 datetime.datetime.fromtimestamp(time.time()))
             with open(
                 self.root_cutdir + '/.log/' +
-                'CVS_ERROR' + ttime + '.log', 'w') as fyle:
+                    'CVS_ERROR' + ttime + '.log', 'w') as fyle:
                 fyle.write('CVS has failed to update.'
-                    ' Please manually debug.')
+                           ' Please manually debug.')
             sys.exit("cvs faild to update")
         return ret
 
@@ -283,9 +286,11 @@ class rootcut(graphbuilder.depbuilder):
             run_type + '/' +
             cut + '/*.root/cutInfoDir/cvsInfo')
         cvs_rev_list = [i.cvsRevision.split('\x00')[0] for i in rev_chain]
-        if not os.path.isdir('{}/{}/{}'.format(root_cut_dir, run_type, cut)): #fixes no directory crash
+        #fixes no directory crash
+        if not os.path.isdir('{}/{}/{}'.format(root_cut_dir, run_type, cut)):
             return '0.0'
-        elif os.listdir('{}/{}/{}'.format(root_cut_dir, run_type, cut)) == []:#fixes empty directory crash
+        #fixes empty directory crash
+        elif os.listdir('{}/{}/{}'.format(root_cut_dir, run_type, cut)) == []:
             return '0.0'
         elif self.check_eq(cvs_rev_list):
             return cvs_rev_list[0]
@@ -316,13 +321,17 @@ class rootcut(graphbuilder.depbuilder):
         error recovery."""
 
         if args:
-            caller ="{}({});exit".format(func, ",".join(["\'{}\'".format(i) for i in args]))
+            caller = "{}({});exit".format(
+                func,
+                ",".join(["\'{}\'".format(i) for i in args]))
         else:
             caller = "{};exit".format(func)
 
         with open(self.root_cutdir_gen + '/.log/MatlabDump_' + self.ttime + '.log', 'a') as fp:
-            ret = subprocess.call(["/usr/local/MATLAB/R2012b/bin/matlab","-nodisplay", "-nosplash", "-r", caller],
-                                  stdout=fp)
+            ret = subprocess.call(
+                ["/usr/local/MATLAB/R2012b/bin/matlab",
+                    "-nodisplay", "-nosplash", "-r", caller],
+                stdout=fp)
         return ret
 
     def make_cut_update_list(self, mat_cutdir, root_cut_list, run_type):
@@ -330,14 +339,14 @@ class rootcut(graphbuilder.depbuilder):
         takes a list of all the old root cuts and returns a list of the cuts that are
         outdated, do not exist, or depend on such a cut."""
 
-        #get new matlab cut list and check for new cuts
+        # get new matlab cut list and check for new cuts
         cuts_to_update = []
         cut_rev_dict = {}
         old_version_inner = {}
         missing_cuts = [j for j in self.new_cut_list if j not in root_cut_list]
-        #make a list of all outdated cuts
+        # make a list of all outdated cuts
         for i in self.new_cut_list:
-            #find revision number of newly updated matlab cuts
+            # find revision number of newly updated matlab cuts
             rev_num_new = self.version_from_cvs(
                 i, self.mat_cutdir)
             cut_rev_dict[i] = rev_num_new
@@ -345,17 +354,17 @@ class rootcut(graphbuilder.depbuilder):
                 cuts_to_update.append((i, rev_num_new))
                 old_version_inner[i] = "*newly created cut*"
             else:
-                #find revision number of old root cuts
+                # find revision number of old root cuts
                 rev_num_old = self.version_from_rootfile(
                     i,
                     self.root_cutdir_gen,
                     run_type)
                 old_version_inner[i] = rev_num_old
-                #print rev_num_new, rev_num_old, (rev_num_new != rev_num_old)
-                #append outdated cut to list
+                # print rev_num_new, rev_num_old, (rev_num_new != rev_num_old)
+                # append outdated cut to list
                 if (rev_num_new != rev_num_old) or self.force:
                     cuts_to_update.append((i, rev_num_new))
-        #deal with reverse deps
+        # deal with reverse deps
         for (i, j) in cuts_to_update:
             for dep in self.parents(i):
                 t = (dep, cut_rev_dict[dep])
@@ -369,11 +378,11 @@ class rootcut(graphbuilder.depbuilder):
 
         with open(
             root_cutdir + "/.log/pickles/" +
-            self.ttime + '.p', 'w') as fyle:
+                self.ttime + '.p', 'w') as fyle:
             pickle.dump(update_dict, fyle)
         with open(
             root_cutdir + '/.log/' +
-            self.ttime + '.log', 'w') as fyle:
+                self.ttime + '.log', 'w') as fyle:
             for h in update_dict:
                 fyle.write(
                     '=========================\n'
@@ -403,8 +412,9 @@ class rootcut(graphbuilder.depbuilder):
         """Calls Ben's hardlink script in a subprocess"""
 
         print "Calling Ben's snapshot script in a subprocess"
-        ret = subprocess.call(["sh", self.root_cutdir_gen + "/../src/take_cuts_snapshot.sh", cutgendir,
-            rootcutdir])
+        ret = subprocess.call(
+            ["sh", self.root_cutdir_gen + "/../src/take_cuts_snapshot.sh", cutgendir,
+             rootcutdir])
         if ret == 1:
             print "Error with Ben's snapshot and hardlink bash script"
 
@@ -417,17 +427,32 @@ class rootcut(graphbuilder.depbuilder):
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("mode", metavar="MODE", nargs='*', help="List of running modes (bg_permitted cf ...) to be rebuilt.")
-    parser.add_argument("-f", "--force", action="store_true", dest="force", default=False, help="Force all cuts to be rebuild")
-    parser.add_argument("-c", "--cuts", help="List of cuts to be rebuilt. Will only build these cuts.", nargs='*')
+    parser.add_argument(
+        "mode",
+        metavar="MODE",
+        nargs='*',
+        help="List of running modes (bg_permitted cf ...) to be rebuilt.")
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        dest="force",
+        default=False,
+        help="Force all cuts to be rebuild")
+    parser.add_argument(
+        "-c",
+        "--cuts",
+        help="List of cuts to be rebuilt. Will only build these cuts.",
+        nargs='*')
     args = parser.parse_args()
     t1 = time.time()
     geterdone = rootcut()
-    if args.mode != None:
+    if args.mode is not None:
         geterdone.run_type_list = args.mode
     geterdone.force = args.force
-    if args.cuts != None:
-        kludge = [(c, geterdone.version_from_cvs(c, geterdone.mat_cutdir)) for c in args.cuts]
+    if args.cuts is not None:
+        kludge = [(c, geterdone.version_from_cvs(c, geterdone.mat_cutdir))
+                  for c in args.cuts]
         geterdone.user_cuts = kludge
     goterdid = geterdone.main()
     t2 = time.time()
