54b6c3e849303cd6ce934e166359cbb541985ddc
Brett Cornell <brett.durcan@gmail.com>
2014-03-27 16:00:18 -0700

    changed domain keyword behavior again

--- graphbuilder.py
+++ graphbuilder.py
@@ -38,7 +38,7 @@ class depbuilder(nx.DiGraph):
                     if '%$depend' in lyne:
                         for k in (j for j in lyne.split() if j in self.cutlist):
                             self.add_edge(i, k)
-                    elif '%$domain' in lyne and 'none' not in lyne:
+                    elif '%$domain' in lyne and 'none' not in lyne.lower():
                         domain_list = lyne.split()[1:]
                         if domain_list == []:
                             domain_list = ['all']
